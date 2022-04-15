8c6fc17005cd40a6235c3e13e36ee5f8ad17ee0d
mounika00650524 <55374518+mounika00650524@users.noreply.github.com>
2022-04-15 15:27:43 +0530

    Added users and admin API

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
 
 app.use(cors());
 app.options('*', cors())
@@ -12,7 +15,8 @@ app.options('*', cors())
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-
+app.use(authJwt());
+app.use(errorHandler);
 
 //Routes
 const categoriesRoutes = require('./routes/categories');
@@ -29,19 +33,19 @@ app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: 'eshop-database'
-    })
-    .then(() => {
-        console.log('Database Connection is ready...')
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
+})
 
 //Server
-app.listen(3000, () => {
+app.listen(3000, ()=>{
 
     console.log('server is running http://localhost:3000');
 })
