fff2fa2ecc1adb7ac5f84c423e361fd054b5435c
mounika00650524 <55374518+mounika00650524@users.noreply.github.com>
2022-04-24 10:04:36 +0530

    File updated

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,24 +9,23 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             `${api}/users/login`,
-            `${api}/users/register`,
+            `${api}/users/register`
+            // { url: /(.*)/ },
         ]
-    })
+    });
 }
-
-// checking for Admin or not to access/preforme any actions as admin
 
 async function isRevoked(req, payload, done) {
     if (!payload.isAdmin) {
-        done(null, true)
+        done(null, true);
     }
 
     done();
 }
 
-
-
-module.exports = authJwt
+module.exports = authJwt;
